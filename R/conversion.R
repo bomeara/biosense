32b14dfc1bd70404adfb4f02f4bcdcd989a23014
Brian O'Meara <omeara.brian@gmail.com>
2016-04-23 10:54:10 -0400

    more skeleton code

--- R/conversion.R
+++ R/conversion.R
@@ -1,13 +1,27 @@
 #' Convert from spectrum to NDSI
 #'
 #' @param spectrum An input spectrum. First column is species names.
+#' @param detector.bounds Two row matrix; each column has frequency bounds for each detector
 #'
 #' @return A data.frame with species names (first column) and NDSI columns
 #' @details
 #' Input columns have colnames that have wavelength in nm (as character), other than species names
 #' Export columns have colnames that have the pair: 700_805 for NDSI for 700 nm and 805 nm bands
+#' Since NDSI should only be computed between spectra measured by the same detector, detector.bounds
+#' provides information on these boundaries. Each column is one detector, first row is starting frequency
+#' measured by that detector, second row is ending frequency. Any frequencies not in the included ranges
+#' will not be included in the output.
 #' @export
-ConvertSpectrumToNDSI <- function(spectrum) {
+ConvertSpectrumToNDSI <- function(spectrum, detector.bounds) {
+}
+
+#' Convert from NDSI vector to matrix
+#'
+#' @param x The measurement vector. Labels are freqA_freqB format
+#'
+#' @return A matrix with freqA on rows, freqB on cols, with NA
+#' @export
+ConvertNDSIVectorToMatrix <- function(x) {
 }
 
 #' Aggregate by species. Return mean and standard error

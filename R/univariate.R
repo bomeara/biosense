9cb598ee903b6127507e7b2b94939138818adc32
Brian O'Meara <omeara.brian@gmail.com>
2016-04-23 11:38:30 -0400

    updates to prediction code

--- R/univariate.R
+++ R/univariate.R
@@ -13,3 +13,22 @@ DoUnivariateOUwieDredge <- function(data, phy) {
 }
 
 #' Get phylogenetic means for species (including ones with missing data)
+#' @param data A data.frame with first column species name, other columms trait data. NA are allowed
+#' @param phy A tree for all species in the data.
+#' @param model A model for phylopars. Options are "BM", "OU" "lambda", "kappa", "delta", "EB", "star"
+#' @param phylo_correlated If TRUE, parameters are estimated assuming traits are correlated. This can be quite slow.
+#' @param ... Other options to pass to phylopars()
+#'
+#' @return list containing means and variances for species, as well as the phylopars object
+#' @details
+#' Note that this will use same model (but different params) for each column.
+#' If you want to use different models, do different subsets of the data
+#' @export
+GetPhylogeneticMeans <- function(data, phy, model="BM", phylo_correlated=FALSE, pad.missing = TRUE, ...) {
+  if(pad.missing) {
+    data <- PadData(data, phy$tip.label, strict=FALSE)
+  }
+  result.phylopars <- phylopars(trait_data = data, tree=phy, model=model, pheno_error=TRUE, phylo_correlated=phylo_correlated)
+  result.list <- list(species.means=result.phylopars$anc_recon[sequence(Ntip(phy)),], species.var=result.phylopars$anc_var[sequence(Ntip(phy)),], result.phylopars = result.phylopars)
+  return(result.list)
+}
